91458ac8dbecdf3484e2d6114aae8ea6386152b5
Caracol <rafaelmeneses@alunos.utfpr.edu.br>
2022-08-19 15:49:26 -0300

    Added procedural button generation

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
+import jsonFile from './pages.json' assert {type: 'json'};
 
+const projectName = document.getElementById("options-menu");
 
+function generateButtons(){
+    for(let i = 0; i < jsonFile.length; i++)
+    {
+        let volumeId = 'volume' + (i + 1);
+        let tempListElement = document.createElement('li');
+        tempListElement.className = "options-list";
 
+        let tempQuestionGroupElement = document.createElement('div')
+        tempQuestionGroupElement.className = "question-group"
+        tempQuestionGroupElement.id = volumeId;
+        tempListElement.appendChild(tempQuestionGroupElement);
+
+        let tempGroupNameElement = document.createElement('p');
+        tempGroupNameElement.className = "group-name";
+        tempGroupNameElement.innerHTML = (i+1) + ". " +jsonFile[i].nome;
+        
+        tempQuestionGroupElement.appendChild(tempGroupNameElement);
+
+        for(let j = 0; j < jsonFile[i].exercicios.length; j++)
+        {
+            let tempButtonElement = document.createElement('button');
+            tempButtonElement.className = "question-selector-button";
+            tempButtonElement.id = volumeId + "exercicio" + j;
+            tempButtonElement.innerHTML = "• " + jsonFile[i].exercicios[j].nome;
+            tempQuestionGroupElement.appendChild(tempButtonElement);
+        }
+
+        
+        projectName.appendChild(tempListElement)
+    }
+}
+generateButtons()
 let markdownString;
 
-fetch('./markdown/1. Seleção Condicional/Exercício 1.1.md')
+fetch('./markdown/Volume1/Exercicio_2/Enunciado_1.md')
    .then(response => response.text())
    .then((response) => {
        getText(response)
